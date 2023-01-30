51ba72de37b65ac52372c0f556c13b4cbae5327a
jaredt <jaredt@jaredts-MBP.attlocal.net>
2023-01-30 17:51:08 -0600

    started database config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,4 +4,14 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
+	database DataBaseSettings
+}
+
+type DataBaseSettings struct {
+	username string
+	password string
+	port string
+	host string
+	database_name string
+	require_ssl string
 }
